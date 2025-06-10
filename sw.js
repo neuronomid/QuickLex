b8a784f11bf8a0b1b82063bb30f0e789af0e9e94
neuronomid <neuron.omid@gmail.com>
2025-06-10 07:50:15 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,6 @@
 self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
 self.addEventListener('fetch', event => {
   const reqUrl = new URL(event.request.url);
   if (reqUrl.origin !== self.location.origin) return;
