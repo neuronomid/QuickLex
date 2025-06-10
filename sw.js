2a18424bbd4ffcdf3139f08746cbe3a41fbf3255
neuronomid <neuron.omid@gmail.com>
2025-06-10 02:34:41 -0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,12 +1,8 @@
 // sw.js
 self.addEventListener('install', e => e.waitUntil(self.skipWaiting()));
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-
-// Only handle same-origin navigation and asset requests
 self.addEventListener('fetch', event => {
   const reqUrl = new URL(event.request.url);
-  // If it’s not your PWA host, don’t intercept it
   if (reqUrl.origin !== self.location.origin) return;
-  // Otherwise just pass through
   event.respondWith(fetch(event.request));
 });
